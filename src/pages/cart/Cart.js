44f98e394b6577a2143c50a941a11fda4b344e94
Sagnik <santrasagnik10@gmail.com>
2023-01-28 19:23:52 +0530

    minor updates

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -63,7 +63,7 @@ const Cart = () => {
   return (
     <section>
       <div className={`container ${styles.table}`}>
-        <h2>Shopping Cart</h2>
+        <h2>My Events</h2>
         {cartItems.length === 0 ? (
           <>
             <p>Your cart is currently empty.</p>
@@ -79,9 +79,9 @@ const Cart = () => {
                 <tr>
                   <th>s/n</th>
                   <th>Product</th>
-                  <th>Price</th>
+                  {/*<th>Price</th>
                   <th>Quantity</th>
-                  <th>Total</th>
+                  <th>Total</th>*/}
                   <th>Action</th>
                 </tr>
               </thead>
@@ -101,7 +101,7 @@ const Cart = () => {
                           style={{ width: "100px" }}
                         />
                       </td>
-                      <td>{price}</td>
+                      {/*<td>{price}</td>
                       <td>
                         <div className={styles.count}>
                           <button
@@ -121,7 +121,7 @@ const Cart = () => {
                           </button>
                         </div>
                       </td>
-                      <td>{(price * cartQuantity).toFixed(2)}</td>
+                      <td>{(price * cartQuantity).toFixed(2)}</td>*/}
                       <td className={styles.icons}>
                         <FaTrashAlt
                           size={19}
@@ -136,22 +136,22 @@ const Cart = () => {
             </table>
             <div className={styles.summary}>
               <button className="--btn --btn-danger" onClick={clearCart}>
-                Clear Cart
+                Clear
               </button>
               <div className={styles.checkout}>
                 <div>
-                  <Link to="/#products">&larr; Continue shopping</Link>
+                  <Link to="/#products">&larr; back to Events</Link>
                 </div>
                 <br />
                 <Card cardClass={styles.card}>
                   <p>
-                    <b> {`Cart item(s): ${cartTotalQuantity}`}</b>
+                    <b> {`Event(s) participating: ${cartTotalQuantity}`}</b>
                   </p>
-                  <div className={styles.text}>
+                  {/*<div className={styles.text}>
                     <h4>Subtotal:</h4>
                     <h3>{`$${cartTotalAmount.toFixed(2)}`}</h3>
-                  </div>
-                  <p>Tax an shipping calculated at checkout</p>
+                  </div>*/}
+                  <p>Lorem ipsum dolor sit amet</p>
                   <button
                     className="--btn --btn-primary --btn-block"
                     onClick={checkout}
